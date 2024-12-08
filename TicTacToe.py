7a08ce739918a64e6ad7b6519d5138eb5cbdbbf5
CNHS Computer Science Club <csclub.cnhs@gmail.com>
2024-12-08 18:18:36 -0500

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,21 @@
+#Starter code for Tic-Tac-Toe project
+
+#true = P1, false = P2
+turn = True
+
+#0 = O, 1 = X
+board = [["", "", ""], 
+         ["", "", ""], 
+         ["", "", ""]]
+         
+#check_board for winners
+def check_board():
+    pass
+
+#creates board per turn
+def new_turn():
+    pass
+
+#resets game
+def reset_game():
+    pass
